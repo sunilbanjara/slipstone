b0eadd76f9c493477a6ddfe5772a992efe6905df
Sunil <Sunil@signitysolutions.com>
2014-02-21 17:43:09 +0530

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Sunil Banjara on 21/02/14.
+//  Copyright (c) 2014 signity macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
